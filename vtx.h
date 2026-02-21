02ade6b96196ecb8770e7d99d9bc358ff38dfaa9
Jack Bolte <jack.bolte@outlook.com>
2026-02-21 12:40:30 -0500

    vtx intial lib

---
+++ vtx.h
@@ -0,0 +1,14 @@
+//https://www.team-blacksheep.com/media/files/tbs_smartaudio_rev08.pdf
+#include "Arduino.h"
+
+class vtx {
+    public:
+        vtx(HardwareSerial* ser);
+        void begin();
+        void getSettings();
+
+    private:
+        HardwareSerial* _ser;
+        uint8_t _crc(uint8_t *data, uint8_t len);
+        void _sendFrame(uint8_t cmd, uint8_t *data, uint8_t len);
+};
