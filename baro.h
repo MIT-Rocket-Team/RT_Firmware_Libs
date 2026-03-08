e6a8664a6c45732b3089c91b6e7ec09900ef9f5d
MDNich <71262643+MDNich@users.noreply.github.com>
2026-03-08 13:10:04 -0400

    Update barometer calibration constants
    Replace the previous C1...C6 calibration macros in baro.h with new hex values and preserve the old values in a commented block for reference. This updates the sensor calibration used by the baro class without changing the API.

--- baro.h
+++ baro.h
@@ -1,12 +1,21 @@
 #include "Arduino.h"
 #include "SPI.h"
 
+/*
 #define C1 0xA579
 #define C2 0x953A
 #define C3 0x68AC
 #define C4 0x6305
 #define C5 0x8405
 #define C6 0x6D91
+*/
+
+#define C1 0xA27A
+#define C2 0x92E4
+#define C3 0x6951
+#define C4 0x61EF
+#define C5 0x91E3
+#define C6 0x6FEC
 
 
 class baro {
