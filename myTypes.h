0e1145e268935242ba076a9c12857532d5c8426c
Jack Bolte <jack.bolte@outlook.com>
2026-03-18 15:08:16 -0400

    move some structs here

--- myTypes.h
+++ myTypes.h
@@ -10,4 +10,22 @@ enum State {
   END
 };
 
+typedef struct __attribute__((packed))
+{
+  int16_t cell1;
+  int16_t cell2;
+  int16_t cell3;
+  int16_t stackVoltage;
+  int16_t current;
+  float temp;
+  uint8_t fetStatus;
+} bmsData;
+
+typedef struct __attribute__((packed))
+{
+  bmsData BMS;
+  int16_t voltages[6];
+  int16_t currents[6];
+} pwrBoardData;
+
 #endif
